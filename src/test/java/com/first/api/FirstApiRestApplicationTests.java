2e2b62d59efa24fc36c6af194988f2c367584f3c
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2025-03-09 10:16:09 -0500

    (UPDATE) add comment to controller

--- src/test/java/com/first/api/FirstApiRestApplicationTests.java
+++ src/test/java/com/first/api/FirstApiRestApplicationTests.java
@@ -2,7 +2,7 @@ package com.first.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//test
 @SpringBootTest
 class FirstApiRestApplicationTests {
 
